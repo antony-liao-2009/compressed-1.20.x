b78e9005ea5f715fd5dfad6a10076724222ae414
antony-liao-2009 <antonyliao2009@outlook.com>
2024-05-14 20:01:29 +0800

    Slight amends to the Iron Tools class + add the "+infinity" damage sign!+remember to add own textures! + CMP_NETHERITEEEEEEEE+CMP_GOLDDDDDDDDDDD!

--- src/main/java/net/antony/compressed/item/ModItems.java
+++ src/main/java/net/antony/compressed/item/ModItems.java
@@ -96,19 +96,19 @@ public class ModItems {
 
     //iron tools
     public static final RegistryObject<Item> COMPRESSED_IRON_SWORD = ITEMS.register("compressed_iron_sword",
-            ()->new SwordItem(ModToolTiers.CMP_DIM,14,
+            ()->new SwordItem(ModToolTiers.CMP_IRON,14,
                     -2.4F,new Item.Properties()));
     public static final RegistryObject<Item> COMPRESSED_IRON_PICKAXE = ITEMS.register("compressed_iron_pickaxe",
-            ()->new PickaxeItem(ModToolTiers.CMP_DIM,3, -2.8F,
+            ()->new PickaxeItem(ModToolTiers.CMP_IRON,3, -2.8F,
                     new Item.Properties()));
     public static final RegistryObject<Item> COMPRESSED_IRON_AXE = ITEMS.register("compressed_iron_axe",
-            ()->new AxeItem(ModToolTiers.CMP_DIM,10,
+            ()->new AxeItem(ModToolTiers.CMP_IRON,10,
                     -3.1F,new Item.Properties()));
     public static final RegistryObject<Item> COMPRESSED_IRON_SHOVEL = ITEMS.register("compressed_iron_shovel",
-            ()->new ShovelItem(ModToolTiers.CMP_DIM,-1,
+            ()->new ShovelItem(ModToolTiers.CMP_IRON,-1,
                     -2.8F,new Item.Properties()));
     public static final RegistryObject<Item> COMPRESSED_IRON_HOE = ITEMS.register("compressed_iron_hoe",
-            ()->new HoeItem(ModToolTiers.CMP_DIM,-1,
+            ()->new HoeItem(ModToolTiers.CMP_IRON,-1,
                     0,new Item.Properties()));
 
 

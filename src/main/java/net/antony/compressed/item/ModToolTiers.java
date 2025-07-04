72b545196afa4b9388d7e36a16a8a18ff1c581bb
Antony <antonyliao2009@outlook.com>
2025-07-04 12:26:34 +0800

    added cmp_gold & cmp_iron textures+ tweaks to mining speed + fix compressed crafting table model texture+ remember to render crafting arrow + remember to add own textures!

--- src/main/java/net/antony/compressed/item/ModToolTiers.java
+++ src/main/java/net/antony/compressed/item/ModToolTiers.java
@@ -13,28 +13,28 @@ import java.util.List;
 
 public class ModToolTiers {
     public static final Tier CMP_DIM = TierSortingRegistry.registerTier(
-            new ForgeTier(5,1561*9,60f,3f,30,
+            new ForgeTier(5,1561*81,65f,3f,20,
                     ModTags.Blocks.NEEDS_CMP_DIM_TOOL,()->Ingredient.of(ModItems.COMPRESSED_DIAMOND.get())),
             new ResourceLocation(Compressed.MODID,"compressed_diamond"),
             List.of(Tiers.NETHERITE),
             List.of());
 
     public static final Tier CMP_IRON = TierSortingRegistry.registerTier(
-            new ForgeTier(4,250*9,14f,3f,20,
+            new ForgeTier(4,250*81,20f,3f,15,
                     ModTags.Blocks.NEEDS_CMP_IRON_TOOL,()->Ingredient.of(ModItems.COMPRESSED_IRON.get())),
             new ResourceLocation(Compressed.MODID,"compressed_iron"),
             List.of(Tiers.DIAMOND),
             List.of());
 
     public static final Tier CMP_GOLD = TierSortingRegistry.registerTier(
-            new ForgeTier(3,40*9,65f,1f,20,
+            new ForgeTier(3,40*18,130f,1f,20,
                     ModTags.Blocks.NEEDS_CMP_GOLD_TOOL,()->Ingredient.of(ModItems.COMPRESSED_GOLD.get())),
             new ResourceLocation(Compressed.MODID,"compressed_gold"),
             List.of(Tiers.GOLD),
             List.of());
 
     public static final Tier CMP_NTHR = TierSortingRegistry.registerTier(
-            new ForgeTier(6,2147483647,1000f,3f,100,
+            new ForgeTier(6,2147483647,2147483647f,3f,25,
                     ModTags.Blocks.NEEDS_CMP_NTHR_TOOL,()->Ingredient.of(ModItems.COMPRESSED_NETHERITE.get())),
             new ResourceLocation(Compressed.MODID,"compressed_netherite"),
             List.of(ModToolTiers.CMP_DIM),
